c104a793b0fae600b993e3c3fb79c1849b2d31cc
Joscelyn <JoscelynRose@gmail.com>
2019-07-22 15:13:15 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import { Route, Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
